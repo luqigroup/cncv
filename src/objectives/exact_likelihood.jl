a0296094a5e598fd7e365e236d5a8e6137f87bf4
Ali Siahkoohi <ali.siahkoohi@gmail.com>
2025-12-20 01:07:37 -0500

    add exact score function

--- src/objectives/exact_likelihood.jl
+++ src/objectives/exact_likelihood.jl
@@ -1,7 +1,7 @@
 # Authors: Ali Siahkoohi, alisk@ucf.edu
 # Date: Nov 2025
 
-export exact_likelihood
+export exact_likelihood, exact_score
 
 
 """
@@ -30,6 +30,52 @@ function exact_likelihood(
     Zx, Zy, logdet = Net.forward(X, Y; logdet_per_batch=true)
     loglike = sum(Distributions.logpdf(Normal(0f0, 1f0), Zx), dims = [1, 2, 3])[1, 1, 1, :]
     loglike = loglike + logdet
+
+end
+
+
+"""
+Compute ∇_X log p(X) given X and Net (score function for unconditional network)
+"""
+function exact_score(Net::NetworkGlow, X::AbstractArray{Float32,4})
+
+    # Forward pass through the network
+    Zx, logdet = Net.forward(X)
+
+    # Gradient of log p(x) w.r.t. Zx
+    # log p(x) = sum(log N(Zx; 0, 1)) + logdet
+    # ∂log p/∂Zx = ∂log N(Zx; 0, 1)/∂Zx
+    ΔZx = gradlogpdf(0.0f0, 1.0f0, Zx)
+
+    # Backward pass to get gradient w.r.t. X
+    ΔX = Net.backward(ΔZx, Zx)[1]
+
+    return ΔX
+
+end
+
+
+"""
+Compute ∇_X log p(X|Y) given X, Y and Net (score function for conditional network)
+"""
+function exact_score(
+    Net::NetworkConditionalGlow,
+    X::AbstractArray{Float32,4},
+    Y::AbstractArray{Float32,4},
+)
+
+    # Forward pass through the network
+    Zx, Zy, logdet = Net.forward(X, Y)
+
+    # Gradient of log p(x|y) w.r.t. Zx
+    # log p(x|y) = sum(log N(Zx; 0, 1)) + logdet
+    # ∂log p/∂Zx = ∂log N(Zx; 0, 1)/∂Zx
+    ΔZx = gradlogpdf(0.0f0, 1.0f0, Zx)
+
+    # Backward pass to get gradient w.r.t. X
+    ΔX = Net.backward(ΔZx, Zx, Zy)[1]
+
+    return ΔX
 
 end
 
